6b27143660723f0a060a9ad7a98b343b56abe8b1
Abhishek Maurya <abhishekmaurya.official@gmail.com>
2024-02-25 16:41:04 +0530

    vercel deployment config fixed

--- src/index.ts
+++ src/index.ts
@@ -23,19 +23,6 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://socialappfrontend.vercel.app"
-  ); // Adjust the origin accordingly
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Access-Control-Allow-Origin"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
 app.use(morgan("common"));
 app.use(express.json());
 app.use(cookieParser());
