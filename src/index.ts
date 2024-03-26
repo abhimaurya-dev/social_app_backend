fcdafc5c535ed82c6ac2a2972c97083447eef102
Abhishek Maurya <abhishekmaurya.official@gmail.com>
2024-03-26 19:22:34 +0530

    vercel deployment fix

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const corsOptions: CorsOptions = {
     "http://localhost:5173",
   ],
   credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["*"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   optionsSuccessStatus: 200,
 };
